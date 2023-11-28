7158b4d0f90d1234431a2934ec3b99cfe82400d5
Oglo <aoglobleff@gmail.com>
2023-11-28 17:20:49 +0100

    Update basicApp.py

--- basicApp.py
+++ basicApp.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
-st.title('Our First app')
+# Chemin vers votre fichier logo (par exemple, 'logo.png')
+logo = "C:\Users\antoi\OneDrive\Bureau\logomigros-removebg-preview.png"
+
+# Utilisation de colonnes pour aligner le logo et le titre
+col1, col2 = st.beta_columns([1, 10])
+
+with col1:
+    st.image(logo, width=50)  # Vous pouvez ajuster la largeur selon vos besoins
+
+with col2:
+    st.title('Team Migros')
+
 st.write('Hello World')
