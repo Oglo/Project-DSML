a4a8dcbb95d041ee3b64d554de83e1a664d5b1a7
Oglo <aoglobleff@gmail.com>
2023-11-28 17:36:27 +0100

    Update basicApp.py

--- Code/basicApp.py
+++ Code/basicApp.py
@@ -1,16 +1,13 @@
 import streamlit as st
 
-# Chemin vers votre fichier logo (par exemple, 'logo.png')
-logo = "C:/Users/antoi/OneDrive/Bureau/logomigros-removebg-preview.png"
+# URL de l'image sur GitHub
+logo_url = "https://raw.githubusercontent.com/user/repo/branch/path/to/image.png"
 
-
-
-
-# Utilisation de colonnes pour aligner le logo et le titr
-col1, col2 = st.beta_columns([1, 10])
+# Utilisation de colonnes pour aligner le logo et le titre
+col1, col2 = st.columns([1, 10])
 
 with col1:
-    st.image(logo, width=50)  # Vous pouvez ajuster la largeur selon vos besoins
+    st.image(logo_url, width=50)  # Ajustez la largeur selon vos besoins
 
 with col2:
     st.title('Team Migros')
