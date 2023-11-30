62c96301ac485a24bb41fa85e8a965de0faeca56
Oglo <aoglobleff@gmail.com>
2023-11-30 14:32:56 +0100

    streamlit changes

--- Code/basicApp.py
+++ Code/basicApp.py
@@ -1,15 +1,25 @@
 import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
 
 # URL de l'image sur GitHub en mode raw
 logo_url = "https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomigros.png"
 
-# Utilisation de colonnes pour aligner le logo et le titre
-col1, col2 = st.columns([1, 10])
+# Télécharger l'image depuis l'URL
+response = requests.get(logo_url)
+logo_img = Image.open(BytesIO(response.content))
+
+# Utilisation de colonnes pour aligner le titre et le logo
+col1, col2 = st.columns([2, 1])
 
 with col1:
-    st.image(logo_url, width=300)  # Ajustez la largeur selon vos besoins
+    # Affichage du titre 'Team'
+    st.markdown("<h1 style='display: inline-block'>Team</h1>", unsafe_allow_html=True)
 
 with col2:
-    st.title('Team Migros')
+    # Affichage du logo
+    st.image(logo_img, width=100)  # Ajustez la largeur selon vos besoins
 
+# Autres éléments de la page
 st.write('Hello World')
--- StartByReadingMe.md
+++ StartByReadingMe.md
@@ -6,5 +6,7 @@ Here you'll find all the files we've worked on to enable us to have a program th
 If you want to learn more about all the work we've done, and how we arrived at this result, I'll let you browse the various folders contained in our GitHub.
 
 
-<img src="https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomail.png" alt="Email Logo" width="35" height="20"> antoine.oglobleff@unil.ch
-<img src="https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomail.png" alt="Email Logo" width="35" height="20"> manuel.solazzo@unil.ch
+You can contact us by e-mail here :
+
+<img src="https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomail.png" alt="Email Logo" width="35" height="20">      antoine.oglobleff@unil.ch
+<img src="https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomail.png" alt="Email Logo" width="35" height="20">      manuel.solazzo@unil.ch
