a64238870e964ebf17802adce23a9fcd0e91c61c
Oglo <aoglobleff@gmail.com>
2023-12-10 22:54:35 +0100

    Update basicApp.py

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -2,24 +2,33 @@ import streamlit as st
 import requests
 import pickle
 import tempfile
+import os
 
 # Télécharger le modèle depuis GitHub et le charger
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_model(url):
-    response = requests.get(url)
-    with tempfile.NamedTemporaryFile(suffix='.pkl') as tmp:
-        tmp.write(response.content)
+    response = requests.get(url, stream=True)
+    if response.status_code != 200:
+        return None
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pkl') as tmp:
+        for chunk in response.iter_content(chunk_size=8192):
+            tmp.write(chunk)
         tmp.flush()
-        model = pickle.load(open(tmp.name, 'rb'))
+        with open(tmp.name, 'rb') as f:
+            model = pickle.load(f)
+    os.remove(tmp.name)  # Supprimer le fichier temporaire
     return model
 
-model_url = 'https://raw.githubusercontent.com/Oglo/Project-DSML/main/Streamlit/language_level_model2.pkl'
+model_url = 'https://github.com/Oglo/Project-DSML/raw/main/Streamlit/language_level_model2.pkl'
 model = load_model(model_url)
 
-# Interface utilisateur Streamlit
-st.title('Prédiction du niveau de langue d’un texte en français')
-text = st.text_area("Entrez votre texte ici:", "")
+if not model:
+    st.error("Erreur lors du chargement du modèle. Veuillez vérifier l'URL ou réessayer plus tard.")
+else:
+    # Interface utilisateur Streamlit
+    st.title('Prédiction du niveau de langue d’un texte en français')
+    text = st.text_area("Entrez votre texte ici:", "")
 
-if st.button('Prédire le niveau de langue'):
-    prediction = model.predict([text])[0]
-    st.write(f'Le niveau de langue prédit pour ce texte est : {prediction}')
+    if st.button('Prédire le niveau de langue'):
+        prediction = model.predict([text])[0]
+        st.write(f'Le niveau de langue prédit pour ce texte est : {prediction}')
