e10dbefeb0f027fe93dff6418a7cc680ed439be6
Oglo <aoglobleff@gmail.com>
2023-12-10 22:34:19 +0100

    l

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -2,15 +2,15 @@ import streamlit as st
 import requests
 import pickle
 from io import BytesIO
-
+import joblib
 # URL du modèle sur GitHub
-url_modele = 'https://github.com/Oglo/Projec-DSML/raw/main/Streamlit/mon_modele_langue.pkl'
+url_modele = 'https://github.com/Oglo/Projec-DSML/raw/main/Streamlit/mon_modele_langue.joblib'
 
 # Fonction pour télécharger et charger le modèle
 def charger_modele(url):
     response = requests.get(url)
     modele = BytesIO(response.content)
-    return pickle.load(modele)
+    return joblib.load(modele)
 
 modele_charge = charger_modele(url_modele)
 
