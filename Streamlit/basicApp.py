546d7c9d67785cf79b03a15a0310f7b013016343
Oglo <aoglobleff@gmail.com>
2023-12-10 22:39:58 +0100

    ,

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -1,28 +1,38 @@
 import streamlit as st
 import requests
-import pickle
-from io import BytesIO
 import joblib
+from io import BytesIO
+
 # URL du modèle sur GitHub
-url_modele = 'https://github.com/Oglo/Projec-DSML/raw/main/Streamlit/mon_modele_langue.joblib'
+url_modele = 'https://github.com/Oglo/Project-DSML/raw/main/Streamlit/mon_modele_langue.joblib'
 
 # Fonction pour télécharger et charger le modèle
 def charger_modele(url):
     response = requests.get(url)
-    modele = BytesIO(response.content)
-    return joblib.load(modele)
+    if response.status_code == 200:
+        modele = BytesIO(response.content)
+        return joblib.load(modele)
+    else:
+        st.error(f"Échec du téléchargement du modèle : statut {response.status_code}")
+        return None
 
+# Télécharge et charge le modèle
 modele_charge = charger_modele(url_modele)
 
-# Fonction pour faire des prédictions
-def predire_niveau(texte):
-    return modele_charge.predict([texte])[0]
+# Vérifie si le modèle a été chargé avec succès
+if modele_charge is not None:
 
-# Interface Streamlit
-st.title('Prédicteur de niveau de langue française')
+    # Fonction pour faire des prédictions
+    def predire_niveau(texte):
+        return modele_charge.predict([texte])[0]
 
-texte_utilisateur = st.text_area("Entrez votre texte ici:")
+    # Interface Streamlit
+    st.title('Prédicteur de niveau de langue française')
 
-if st.button('Prédire'):
-    niveau_predi = predire_niveau(texte_utilisateur)
-    st.write(f'Le niveau de langue prédit est : {niveau_predi}')
+    texte_utilisateur = st.text_area("Entrez votre texte ici:")
+
+    if st.button('Prédire'):
+        niveau_predi = predire_niveau(texte_utilisateur)
+        st.write(f'Le niveau de langue prédit est : {niveau_predi}')
+else:
+    st.error("Le modèle n'a pas pu être chargé. Veuillez vérifier l'URL du modèle.")
