47562b7a247bdf2a92578a5055e52ecbe2fa20d0
Oglo <aoglobleff@gmail.com>
2023-12-10 20:31:26 +0100

    Update basicApp.py

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -1,17 +1,7 @@
+import Streamlit
 import requests
 import joblib
 from io import BytesIO
-
-# URL du modèle sur GitHub
-model_url = "https://github.com/Oglo/Project-DSML/raw/main/Streamlit/model_langue.pkl"
-label_encoder_url = "https://github.com/Oglo/Project-DSML/raw/main/Streamlit/label_encoder.pkl"
-
-def load_model_from_github(url):
-    response = requests.get(url)
-    model = joblib.load(BytesIO(response.content))
-    return model
-
-
 
 # URL de l'image sur GitHub en mode raw
 logo_url = "https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomigros.png"
@@ -85,5 +75,26 @@ if st.button("Prédire le niveau de langue"):
         prediction = model.predict([user_text])
         predicted_level = label_encoder.inverse_transform(prediction)
         st.write(f"Niveau de langue prédit : {predicted_level[0]}")
+    else:
+        st.write("Veuillez entrer un texte.")
+
+        # Téléchargement du modèle depuis GitHub
+model_url = "https://github.com/Oglo/Project-DSML/raw/main/Streamlit/model_langue.pkl"
+response = requests.get(model_url)
+model_file = BytesIO(response.content)
+model = joblib.load(model_file)
+
+# Création d'un champ de saisie pour le texte à analyser
+user_text = st.text_area("Entrez votre texte ici :")
+
+# Bouton pour effectuer la prédiction
+if st.button('Prédire le niveau de langue'):
+    if user_text:
+        # Prédiction
+        predicted_level = model.predict([user_text])
+        # Traduction du niveau de langue en termes compréhensibles
+        level_mapping = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
+        predicted_level_text = level_mapping[predicted_level[0]]
+        st.write(f"Le niveau de langue prédit pour le texte est : {predicted_level_text}")
     else:
         st.write("Veuillez entrer un texte.")
