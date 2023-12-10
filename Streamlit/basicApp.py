057579507473fa8967994332d21e3661b55fc291
Oglo <aoglobleff@gmail.com>
2023-12-10 22:28:12 +0100

    c

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -1,27 +1,18 @@
 import streamlit as st
-import requests
-from PIL import Image
-from io import BytesIO
 import pickle
 
+# Charger le modèle
+with open('mon_modele_langue.pkl', 'rb') as file:
+    modele_charge = pickle.load(file)
 
-# Téléchargement du modèle depuis GitHub
-model_url = "https://github.com/Oglo/Project-DSML/raw/main/Streamlit/model_langue.pkl"
-response = requests.get(model_url)
-model_file = BytesIO(response.content)
-model = pickle.load(model_file)
+def predire_niveau(texte):
+    return modele_charge.predict([texte])[0]
 
-# Création d'un champ de saisie pour le texte à analyser
-user_text = st.text_area("Entrez votre texte ici :")
+# Interface Streamlit
+st.title('Prédicteur de niveau de langue française')
 
-# Bouton pour effectuer la prédiction
-if st.button('Prédire le niveau de langue'):
-    if user_text:
-        # Prédiction
-        predicted_level = model.predict([user_text])
-        # Traduction du niveau de langue en termes compréhensibles
-        level_mapping = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
-        predicted_level_text = level_mapping[predicted_level[0]]
-        st.write(f"Le niveau de langue prédit pour le texte est : {predicted_level_text}")
-    else:
-        st.write("Veuillez entrer un texte.")
+texte_utilisateur = st.text_area("Entrez votre texte ici:")
+
+if st.button('Prédire'):
+    niveau_predi = predire_niveau(texte_utilisateur)
+    st.write(f'Le niveau de langue prédit est : {niveau_predi}')
