185b3f7e53d0d2a1652e9f36769bc6b491d56214
Oglo <aoglobleff@gmail.com>
2023-12-11 20:53:38 +0100

    l

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -6,6 +6,8 @@ import sklearn
 import tempfile
 import requests
 from io import BytesIO
+from PIL import Image
+
 
 # Fonction pour charger le modèle depuis GitHub
 def load_model(url):
@@ -14,7 +16,19 @@ def load_model(url):
     return model
 
 # Interface Streamlit
-st.title('Prédiction du niveau de langue')
+logo_url = "https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomigros.png"
+
+# Télécharger l'image depuis l'URL
+response = requests.get(logo_url)
+logo_img = Image.open(BytesIO(response.content))
+
+# Création d'une structure de colonnes pour aligner le titre et l'image
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:  # Utilisation de la colonne centrale
+    # Affichage du titre 'Team' centré
+    st.markdown("<h1 style='text-align: center'>Team</h1>", unsafe_allow_html=True)    
+    st.image(logo_img)
 
 # Sélection du pourcentage de précision
 precision = st.selectbox('Choisissez le pourcentage de précision :', 
@@ -23,9 +37,9 @@ precision = st.selectbox('Choisissez le pourcentage de précision :',
 # Afficher les modèles en fonction de la précision choisie
 # Cela dépend de la manière dont vous associez les pourcentages aux modèles
 if precision == '30%':
-    models = ['Logistic_Regression', 'Modèle B']  # Exemple
+    models = ['Model A', 'Modèle B']  # Exemple
 elif precision == '40%':
-    models = ['Modèle C', 'Modèle D']  # Exemple
+    models = ['Logistic Regression (45%)', 'Modèle D']  # Exemple
 # Ajoutez d'autres conditions pour les autres pourcentages
 
 model_choice = st.selectbox('Choisissez un modèle :', models)
