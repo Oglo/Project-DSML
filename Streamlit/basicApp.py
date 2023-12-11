20d35dbc61b4cb0573b9b634ae8ac921c95bbfa7
Oglo <aoglobleff@gmail.com>
2023-12-11 20:49:43 +0100

    l

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -4,36 +4,38 @@ from joblib import load
 import joblib
 import sklearn
 import tempfile
+import requests
+from io import BytesIO
 
-# Fonction pour télécharger le modèle depuis GitHub
-def download_model(url):
-    r = requests.get(url, allow_redirects=True)
-    if r.status_code == 200:
-        f = tempfile.NamedTemporaryFile(delete=False, suffix='.joblib')
-        f.write(r.content)
-        f.close()
-        return f.name
-    else:
-        return None
-
-# URL du modèle sur GitHub
-model_url = 'https://github.com/Oglo/Project-DSML/raw/main/Streamlit/language_level_classifier.joblib'
-
-# Télécharger le modèle
-model_path = download_model(model_url)
-if model_path:
-    model = joblib.load(model_path)
-else:
-    st.error("Erreur lors du téléchargement du modèle")
-    st.stop()
+# Fonction pour charger le modèle depuis GitHub
+def load_model(url):
+    response = requests.get(url)
+    model = joblib.load(BytesIO(response.content))
+    return model
 
 # Interface Streamlit
-st.title("Prédiction du Niveau de Langue Française")
-text = st.text_area("Entrez votre texte ici:")
+st.title('Prédiction du niveau de langue')
 
-if st.button('Prédire'):
-    if text:
-        prediction = model.predict([text])[0]
-        st.success(f"Le niveau de langue prédit est: {prediction}")
-    else:
-        st.error("Veuillez entrer un texte.")
+# Sélection du pourcentage de précision
+precision = st.selectbox('Choisissez le pourcentage de précision :', 
+                         ['30%', '40%', '50%', '55%', '60%'])
+
+# Afficher les modèles en fonction de la précision choisie
+# Cela dépend de la manière dont vous associez les pourcentages aux modèles
+if precision == '30%':
+    models = ['Logistic_Regression', 'Modèle B']  # Exemple
+elif precision == '40%':
+    models = ['Modèle C', 'Modèle D']  # Exemple
+# Ajoutez d'autres conditions pour les autres pourcentages
+
+model_choice = st.selectbox('Choisissez un modèle :', models)
+
+# Zone de texte pour la saisie de la phrase
+user_input = st.text_area("Entrez votre texte ici:")
+
+# Chargement et prédiction du modèle
+if st.button('Prédire le niveau de langue'):
+    model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+    model = load_model(model_url)
+    prediction = model.predict([user_input])
+    st.write(f'Niveau de langue prédit: {prediction[0]}')
