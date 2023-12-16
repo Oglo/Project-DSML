4a0890319395419f8da440e20035db1cafeea12f
Oglo <aoglobleff@gmail.com>
2023-12-16 15:28:39 +0100

    Update basicApp.py

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -5,6 +5,7 @@ from io import BytesIO
 from PIL import Image
 import spacy
 import pandas as pd
+import joblib
 
 # Fonction pour charger le modèle depuis GitHub
 def load_model(url):
@@ -21,6 +22,19 @@ def extract_features(text):
     avg_sentence_length = num_tokens / num_sentences if num_sentences > 0 else 0
     lexical_diversity = len(set(token.text.lower() for token in doc)) / num_tokens if num_tokens > 0 else 0
     return [avg_sentence_length, lexical_diversity]
+
+# Fonction pour prédire avec Logistic Regression
+def predict_with_logistic_regression(model, text):
+    features = extract_features(text)
+    features_df = pd.DataFrame([features], columns=['avg_sentence_length', 'lexical_diversity'])
+    prediction = model.predict(features_df)
+    return prediction
+
+# Fonction pour prédire avec SVC
+def predict_with_svc(model, vectorizer, text):
+    processed_text = vectorizer.transform([text])
+    prediction = model.predict(processed_text)
+    return prediction
 
 # Interface Streamlit
 logo_url = "https://raw.githubusercontent.com/Oglo/Project-DSML/main/Code/images/logomigros.png"
@@ -39,14 +53,14 @@ with col2:  # Utilisation de la colonne centrale
 # Sélection du pourcentage de précision
 precision = st.selectbox('Choisissez le pourcentage de précision :', ['30%', '40%', '45%', '55%', '60%'])
 
-# Afficher les modèles en fonction de la précision choisie
-# Cela dépend de la manière dont vous associez les pourcentages aux modèles
+# Charger le modèle et le vectorizer si nécessaire
+model_choice = None
 if precision == '30%':
     models = ['Random Forest (36%)', 'Spacy (32%)']
 elif precision == '45%':
     models = ['Logistic Regression (45%)', 'RNN']
-elif precision == '40%' :
-    models = ['Support Vector Machine (41,5%)']
+elif precision == '40%':
+    models = ['Vector']
 # Ajoutez d'autres conditions pour les autres pourcentages
 
 model_choice = st.selectbox('Choisissez un modèle :', models)
@@ -56,13 +70,17 @@ user_input = st.text_area("Entrez votre texte ici:")
 
 # Chargement et prédiction du modèle
 if st.button(f'Prédire le niveau de langue avec {model_choice}'):
-    model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
-    model = load_model(model_url)
-
-    # Transformer l'entrée utilisateur en caractéristiques numériques
-    user_input_features = extract_features(user_input)
-    user_input_df = pd.DataFrame([user_input_features], columns=['avg_sentence_length', 'lexical_diversity'])
-    prediction = model.predict(user_input_df)
+    if 'Logistic Regression (45%)' in model_choice:
+        model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+        model = load_model(model_url)
+        prediction = predict_with_logistic_regression(model, user_input)
+    elif 'Vector' in model_choice:
+        model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+        vectorizer_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/vectorizer.joblib"  # Assurez-vous que cette URL est correcte
+        model = load_model(model_url)
+        vectorizer = joblib.load(BytesIO(requests.get(vectorizer_url).content))
+        prediction = predict_with_svc(model, vectorizer, user_input)
+    # Ajoutez d'autres conditions pour les autres modèles si nécessaire
 
     # Dictionnaire pour mapper les chiffres aux niveaux de langue
     niveau_langue = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
