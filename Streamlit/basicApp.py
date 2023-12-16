0f11542833c23db704ad2e1370bf269f59f273a3
Oglo <aoglobleff@gmail.com>
2023-12-16 18:13:54 +0100

    c

--- Streamlit/basicApp.py
+++ Streamlit/basicApp.py
@@ -7,7 +7,32 @@ import spacy
 import pandas as pd
 import joblib
 import youtube_dl
+import re
+from pytube import YouTube
 #import tensorflow
+
+
+def download_youtube_subtitles(url):
+    yt = YouTube(url)
+    caption = yt.captions.get_by_language_code('fr')
+    if caption:
+        return caption.generate_srt_captions()
+    else:
+        return "Aucun sous-titre disponible"
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        # Charger et retourner le contenu du fichier de sous-titres
+        with open('temp_subs.fr.vtt', 'r', encoding='utf-8') as file:
+            subtitles = file.read()
+    return subtitles
+
+# Fonction pour nettoyer les sous-titres
+def clean_subtitles(subtitles):
+    # Suppression des balises et des timestamps
+    cleaned = re.sub(r'<[^>]+>', '', subtitles)
+    cleaned = re.sub(r'\d{2}:\d{2}:\d{2}.\d{3} --> \d{2}:\d{2}:\d{2}.\d{3}', '', cleaned)
+    return cleaned
 
 # Dictionnaire pour mapper les chiffres aux niveaux de langue
 niveau_langue = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
@@ -114,4 +139,38 @@ if st.button(f'Prédire le niveau de langue avec {model_choice}'):
 
 # Importations supplémentaires
 
+youtube_url = st.text_input("Collez l'URL de la vidéo YouTube ici :")
 
+# Bouton pour déclencher le téléchargement et la prédiction
+if st.button("Extraire et prédire le niveau de langue"):
+    try:
+        subtitles = download_youtube_subtitles(youtube_url)
+        cleaned_subtitles = clean_subtitles(subtitles)
+        
+        # Utiliser le modèle choisi pour prédire le niveau de langue
+        if 'Logistic Regression (45%)' in model_choice:
+            model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+            model = load_model(model_url)
+            prediction = predict1(model, cleaned_subtitles)
+        elif 'Vector' in model_choice:
+            model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+            vectorizer_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/vectorizer.joblib"
+            model = load_model(model_url)
+            vectorizer = joblib.load(BytesIO(requests.get(vectorizer_url).content))
+            prediction = predict2(model, vectorizer, cleaned_subtitles)
+        elif 'Spacy (32%)' in model_choice:
+            model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+            model = load_model(model_url)
+            prediction = predict1(model, cleaned_subtitles)
+        elif 'Random Forest' in model_choice:
+            model_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/{model_choice}.joblib"
+            vectorizer_url = f"https://github.com/Oglo/Project-DSML/raw/main/Streamlit/vectorizer.joblib"
+            model = load_model(model_url)
+            vectorizer = joblib.load(BytesIO(requests.get(vectorizer_url).content))
+            prediction = predict2(model, vectorizer, cleaned_subtitles)
+
+        # Afficher le niveau de langue prédit
+        predicted_level = niveau_langue.get(int(prediction[0]), "Niveau inconnu")
+        st.write(f'Niveau de langue prédit: {predicted_level}')
+    except Exception as e:
+        st.error(f"Une erreur s'est produite : {e}")
